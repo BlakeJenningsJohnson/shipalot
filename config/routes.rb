4a35029aa876ca9fbf06eff00206173e3e341d1e
Your Name <blake.jennings@gmail.com>
2014-02-26 14:22:53 -0800

    route example

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Shipalot::Application.routes.draw do
+
+  get '/request', to: 'requests#request'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

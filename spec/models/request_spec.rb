d0fe28e44d4b34d061efbe4bd9e67fdb9c25cfae
Your Name <blake.jennings@gmail.com>
2014-02-26 16:05:25 -0800

    parse request spec

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Request do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "parse_request_parameters" do
+    let!(:params){ ['Seattle', 'US', 'WA', '98112',
+                  'US', 'Englewood', 'CO', '80111',
+                  '100', '10', '14', '4.5'] }
+
+    it "sets the origin" do
+      request = Request.parse_request_parameters(*params)
+      expect(request.origin).to be_an_instance_of(ActiveMerchant::Shipping::Location)
+    end
+  end
 end

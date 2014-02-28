2a30f2067ba8b45654f7dc0bb524f3bae0da9d1f
Davida Marion <davidamarion@gmail.com>
2014-02-27 16:31:35 -0800

    working though api call

--- app/models/shipping_log.rb
+++ app/models/shipping_log.rb
@@ -1,59 +1,47 @@
 class ShippingLog  < ActiveRecord::Base
   include ActiveMerchant::Shipping
-  before_action :set_ups_client
 
   attr_reader :origin, :package, :destination
 
-  def self.make_api_call(*args)
-    parse_request_parameters(*args)
-    ups_response = ups.find_rates(@origin, @destination, @package)
-    ups_response.parse_ups_rates
-    # usps.find_rates(@origin, @destination, @package)
-    # find_ups_rates(new_ups_client)
-
-    # fedex.find_rates . . .
+  def self.make_api_call(params_arg)
+    parse_request_parameters(params_arg)
+    ups = set_ups_client
+    ups_response = ups.find_rates(@origin, @destination, @package).rates
+    parse_ups_rates
   end
 
   def self.parse_request_parameters(
-        origin_city, origin_country, origin_state, origin_zip,
-        destination_country, destination_city, destination_state, destination_zip,
-        package_weight, package_height, package_depth, package_length
+        params_arg
       )
-      # current_request = self.new #self is optional here
-    @origin = set_origin(origin_country, origin_state, origin_city, origin_zip)
-    @destination = set_destination(destination_country, destination_state, destination_city, destination_zip)
-    @package = set_package(package_weight.to_i, [package_height.to_f, package_depth.to_f, package_length.to_f])
+    # @package = set_package(package_weight.to_i, [package_height.to_f, package_depth.to_f, package_length.to_f])
+    @origin = set_origin(params_arg[:origin])
+    @destination = set_destination(params_arg[:destination])
+    @package = set_package(params_arg[:package])
       
   end
 
 
   def parse_ups_rates
     # response =  ups_client.find_rates(@origin, @destination, @package)
-   self.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_date]}
+   rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_date]}
   end
 
   private
 
-  def self.set_origin(origin_country, origin_state, origin_city, origin_zip)
-    Location.new(country: origin_country,
-                        state: origin_state,
-                        city: origin_city,
-                        zip: origin_zip)
+  def self.set_origin(origin_info)
+    Location.new(origin_info)
   end
 
-  def self.set_package(package_weight, package_dimensions)
-    Package.new(package_weight, package_dimensions) #this is in grams and LxWxD
+  def self.set_package(package_info)
+    Package.new(package_info[:weight], package_info[:dimensions]) #this is in grams and LxWxD
   end
 
-  def self.set_destination(destination_country, destination_city, destination_state, destination_zip)
-    Location.new(country: destination_country,
-                                  state: destination_state,
-                                   city: destination_city,
-                                    zip: destination_zip)
+  def self.set_destination(destination_info)
+    Location.new(destination_info)
   end
 
   def self.set_ups_client
-    ups = UPS.new(login: ENV['UPS_LOGIN'], 
+    UPS.new(login: ENV['UPS_LOGIN'],
                   password: ENV['UPS_PASSWORD'],
                   key: ENV['UPS_KEY'])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Shipalot::Application.routes.draw do
 
-  post '/request/', to: 'shippinglogs#request'
+  post '/request', to: 'shipping_logs#shipping_info'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

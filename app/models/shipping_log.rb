f4dc6f7471b788f60be836c1e1cc5950bcaf6652
Your Name <blake.jennings@gmail.com>
2014-03-03 13:15:06 -0800

    consolidated a buncha methods in the model

--- app/models/shipping_log.rb
+++ app/models/shipping_log.rb
@@ -3,26 +3,11 @@ class ShippingLog  < ActiveRecord::Base
 
   attr_reader :origin, :package, :destination
 
-# Location.should_receive(:new).with(origin)
-
   def self.assign_data_and_call(params_arg, carrier)
     @origin = Location.new(params_arg[:origin])
-    @destination = parse_destination_parameters(params_arg[:destination])
-    @package = parse_package_parameters(params_arg[:package])
+    @destination = Location.new(params_arg[:destination])
+    @package = Package.new(params_arg[:package][:weight], params_arg[:package][:dimensions])
     make_call(@origin, @destination, @package, carrier)
-  end
-
-  def self.parse_origin_parameters(origin_params)
-    # set_origin(origin_params)
-    Location.new(origin_params)
-  end
-
-  def self.parse_destination_parameters(destination_params)
-    set_destination(destination_params)
-  end
-
-  def self.parse_package_parameters(package_params)
-    set_package(package_params)
   end
 
   def self.make_call(origin, destination, package, carrier) #self method?
@@ -39,29 +24,21 @@ class ShippingLog  < ActiveRecord::Base
 
   private
 
-  def self.set_origin(origin_info)
-    Location.new(origin_info)
-  end
-
-  def self.set_package(package_info)
-    Package.new(package_info[:weight], package_info[:dimensions]) #this is in grams and LxWxD
-  end
-
-  def self.set_destination(destination_info)
-    Location.new(destination_info)
-  end
-
   def self.ups
-    UPS.new(login: ENV['UPS_LOGIN'],
-                  password: ENV['UPS_PASSWORD'],
-                  key: ENV['UPS_KEY'])
+    UPS.new(
+      login: ENV['UPS_LOGIN'],
+      password: ENV['UPS_PASSWORD'],
+      key: ENV['UPS_KEY']
+      )
   end
 
   def self.fedex
-    FedEx.new(:login => ENV['FEDEX_METER'],
-              :password => ENV['FEDEX_PASSWORD'], 
-              key: ENV['FEDEX_KEY'], 
-              account: ENV['FEDEX_ACCOUNT'], 
-              :test => true)
+    FedEx.new(
+      login: ENV['FEDEX_METER'],
+      password: ENV['FEDEX_PASSWORD'],
+      key: ENV['FEDEX_KEY'],
+      account: ENV['FEDEX_ACCOUNT'],
+      test: true
+      )
   end
 end

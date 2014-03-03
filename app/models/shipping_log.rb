831baa52814b6dd595aaa71d0469af35b8781f2d
Your Name <blake.jennings@gmail.com>
2014-03-03 10:18:52 -0800

    deleted old code and comments

--- app/models/shipping_log.rb
+++ app/models/shipping_log.rb
@@ -2,10 +2,6 @@ class ShippingLog  < ActiveRecord::Base
   include ActiveMerchant::Shipping
 
   attr_reader :origin, :package, :destination
-  #make_ups_call needs logic from make_api_call
-  #add method in controller to do call, which is separate from parsing params? (I think this could still be one method, but I'll separate them for now)
-  #add make_fedex_call 
-  #add make_all_the_calls
 
   def self.assign_data_and_call(params_arg, carrier)
     @origin = parse_origin_parameters(params_arg[:origin])
@@ -13,13 +9,6 @@ class ShippingLog  < ActiveRecord::Base
     @package = parse_package_parameters(params_arg[:package])
     make_call(@origin, @destination, @package, carrier)
   end
-
-  # def self.fedex_call(params_arg)
-  #   @origin = parse_origin_parameters(params_arg[:origin])
-  #   @destination = parse_destination_parameters(params_arg[:destination])
-  #   @package = parse_package_parameters(params_arg[:package])
-  #   make_fedex_call(@origin, @destination, @package)
-  # end
 
   def self.parse_origin_parameters(origin_params)
     set_origin(origin_params)
@@ -33,11 +22,6 @@ class ShippingLog  < ActiveRecord::Base
     set_package(package_params)
   end
 
-  # def self.parse_request_parameters(params_arg)
-  #   @destination = set_destination(params_arg[:destination])
-  #   @package = set_package(params_arg[:package])    
-  # end
-  
   def self.make_call(origin, destination, package, carrier) #self method?
     client = self.send(carrier.to_sym)
     returned_rates = client.find_rates(origin, destination, package).rates
@@ -47,17 +31,6 @@ class ShippingLog  < ActiveRecord::Base
   def self.parse_rates(any_rates)
    any_rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_date]}
   end
-
-  # def self.make_fedex_call(origin, destination, package) #self method?
-  #   fedex = set_fedex_client
-  #   fedex_rates = fedex.find_rates(origin, destination, package).rates
-  #   parse_rates(fedex_rates)
-  # end
-
-  # def self.parse_fedex_rates(fedex_rates)
-  #   # response =  ups_client.find_rates(@origin, @destination, @package)
-  #  fedex_rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_date]}
-  # end
 
   private
 
@@ -86,8 +59,4 @@ class ShippingLog  < ActiveRecord::Base
               account: ENV['FEDEX_ACCOUNT'], 
               :test => true)
   end
-  # def self.usps
-  #   # usps = USPS.new(login: )
-  # end
-
 end

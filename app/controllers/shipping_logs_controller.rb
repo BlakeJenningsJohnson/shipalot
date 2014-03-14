6ac4d39fa5cc22ad85c888fe39b042f1d6ac1671
Davida Marion <davidamarion@gmail.com>
2014-03-14 11:24:45 -0700

    updates testing comment

--- app/controllers/shipping_logs_controller.rb
+++ app/controllers/shipping_logs_controller.rb
@@ -29,3 +29,6 @@ end
 
 # to test w/HTTParty, you can add these options:
 # options = { origin: {country:  'US', state:  'CA', city:  'Beverly Hills', zip:  '90210'}, destination: {country: 'US', state: 'WA', city:  'Seattle', zip:  '98122' }, package: { weight: 100, dimensions: [5, 7, 6] } }
+
+# HTTParty.post("http://shipalot.herokuapp.com/ups.json", body: options.to_json, headers: {'Content-Type' => 'application/json'})
+
